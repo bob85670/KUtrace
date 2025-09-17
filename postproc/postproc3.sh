977ce23229551a68f2be4daebb03230356e156e6
ubuntu <bob85670@connect.hku.hk>
2025-09-17 15:47:31 +0800

    the output files are now in a separate folder

--- postproc/postproc3.sh
+++ postproc/postproc3.sh
@@ -7,17 +7,37 @@ export LC_ALL=C
 # Strip trailing .trace if it is there
 var1=${1%.trace}
 
-cat $var1.trace  |./rawtoevent |sort -n |./eventtospan3 "$2" |sort >$var1.json 
-echo "  $var1.json written"
-
-trim_arg='0'
-if [ -n "$3" ]
-then
-delimit=' '
-trim_arg=$3$delimit$4
+# Extract date and time from filename (e.g., ku_20240709_152922_dclab-2_11686.trace)
+# Assuming filename starts with ku_YYYYMMDD_HHMMSS
+filename=$(basename "$var1")
+if [[ $filename =~ ku_([0-9]{8})_([0-9]{6}) ]]; then
+    date_part=${BASH_REMATCH[1]}
+    time_part=${BASH_REMATCH[2]}
+    output_dir="ku_${date_part}_${time_part}"
+else
+    echo "Error: Filename does not match expected pattern ku_YYYYMMDD_HHMMSS..."
+    exit 1
 fi
 
-cat $var1.json |./spantotrim $trim_arg |./makeself show_cpu.html >$var1.html
-echo "  $var1.html written"
+# Create the output directory
+mkdir -p "$output_dir"
 
-google-chrome $var1.html &
+# Process the trace file and output to the new directory
+cat "$var1.trace" | ./rawtoevent | sort -n | ./eventtospan3 "$2" | sort > "$output_dir/$filename.json"
+echo "  $output_dir/$filename.json written"
+
+trim_arg='0'
+if [ -n "$3" ]; then
+    delimit=' '
+    trim_arg=$3$delimit$4
+fi
+
+cat "$output_dir/$filename.json" | ./spantotrim $trim_arg | ./makeself show_cpu.html > "$output_dir/$filename.html"
+echo "  $output_dir/$filename.html written"
+
+# Move the original trace file to the output directory
+mv "$var1.trace" "$output_dir/$filename.trace"
+echo "  $output_dir/$filename.trace moved"
+
+# Open the HTML file in Google Chrome
+google-chrome "$output_dir/$filename.html" &
